4ae768ab1baa93149f8e74a9d598d45041572875
苏彤 <sutong@jj.mail.com>
2024-07-11 09:58:35 +0800

    http和grpc测试

--- service/user/test/user.go
+++ service/user/test/user.go
@@ -2,43 +2,84 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/grpc"
-	v1 "user/api/user/v1"
+	"log"
+	"time"
+
+	v1 "user/api/user/v1" // 导入 user client
+
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/hashicorp/consul/api"
 )
 
-var userClient v1.UserClient
-var conn *grpc.ClientConn
-
 func main() {
+    c := api.DefaultConfig()
+    c.Address = "192.168.29.130:8500"
+    c.Scheme = "http"
+    consulCli, err := api.NewClient(c)
+    if err != nil {
+        panic(err)
+    }
+    r := consul.New(consulCli)
 
-	Init()
+    // new grpc client
+    conn, err := grpc.DialInsecure(
+        context.Background(),
+        grpc.WithEndpoint("discovery:///user"),
+        grpc.WithDiscovery(r),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+    gClient := v1.NewUserClient(conn) // 使用 UserClient
 
-	TestCreateUser() // 创建用户
+    // new http client
+    hConn, err := http.NewClient(
+        context.Background(),
+        http.WithMiddleware(
+            recovery.Recovery(),
+        ),
+        http.WithEndpoint("discovery:///user"),
+        http.WithDiscovery(r),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer hConn.Close()
+    hClient := v1.NewUserHTTPClient(hConn) // 使用 UserHTTPClient
 
-	conn.Close()
+    for {
+        time.Sleep(time.Second)
+        callGRPC(gClient)
+        callHTTP(hClient) // 调用 HTTP 客户端
+    }
 }
 
-// Init 初始化 grpc 链接 注意这里链接的 端口
-func Init() {
-	var err error
-	conn, err = grpc.Dial("127.0.0.1:50051", grpc.WithInsecure())
-	if err != nil {
-		panic("grpc link err" + err.Error())
-	}
-
-	userClient = v1.NewUserClient(conn)
+func callGRPC(client v1.UserClient) {
+    // 这里调用 UserClient 的方法，例如 UpdateUser
+    rsp, err := client.UpdateUser(context.Background(), &v1.UpdateUserRequest{
+        Mobile:   "138110110",
+        Password: "password",
+        NickName: "nic11kna44me",
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("[grpc] UpdateUser %+v\n", rsp)
 }
 
-func TestCreateUser() {
-
-	rsp, err := userClient.CreateUser(context.Background(), &v1.CreateUserRequest{
-		Mobile:   fmt.Sprintf("138324888%d", 1),
-		Password: "admin123",
-		NickName: fmt.Sprintf("YWW3W%d", 1),
-	})
-	if err != nil {
-		panic("grpc 创建用户失败" + err.Error())
-	}
-	fmt.Println(rsp.Id)
+func callHTTP(client v1.UserHTTPClient) {
+    // 这里调用 UserHTTPClient 的方法，例如 UpdateUser
+    rsp, err := client.UpdateUser(context.Background(), &v1.UpdateUserRequest{
+        Mobile:   "138110110",
+        Password: "password",
+        NickName: "nic11kna44me",
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("[http] UpdateUser %+v\n", rsp)
 }
